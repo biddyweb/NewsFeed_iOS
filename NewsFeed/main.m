db8cc140868ae460cc7a4ad039dbd7232d985325
Michael Hoffstaedter <michael.hoffstaedter@kaasa.com>
2015-07-31 12:26:31 +0200

    Initial Commit

---
+++ NewsFeed/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewsFeed
+//
+//  Created by AlexKern on 31.07.15.
+//  Copyright (c) 2015 Kaasa Solution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
